4d633b89e1ff637428dc110d6757151cf33ba804
Sumit Jangir <sumj@saxobank.com>
2020-04-09 16:31:18 +0530

    first commit

---
+++ CSVBreaker/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace CSVBreaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtInputFileDelimeter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputFilePath = new System.Windows.Forms.TextBox();
+            this.lblInputPath = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtNoOfrecords = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtOutputFileDelimeter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutputFilePath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.prgsBar = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.txtInputFileDelimeter);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtInputFilePath);
+            this.panel1.Controls.Add(this.lblInputPath);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtInputFileDelimeter
+            // 
+            this.txtInputFileDelimeter.Location = new System.Drawing.Point(148, 58);
+            this.txtInputFileDelimeter.Name = "txtInputFileDelimeter";
+            this.txtInputFileDelimeter.Size = new System.Drawing.Size(143, 20);
+            this.txtInputFileDelimeter.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label1.Location = new System.Drawing.Point(20, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input File Delimeter";
+            // 
+            // txtInputFilePath
+            // 
+            this.txtInputFilePath.Location = new System.Drawing.Point(148, 13);
+            this.txtInputFilePath.Name = "txtInputFilePath";
+            this.txtInputFilePath.Size = new System.Drawing.Size(603, 20);
+            this.txtInputFilePath.TabIndex = 1;
+            // 
+            // lblInputPath
+            // 
+            this.lblInputPath.AutoSize = true;
+            this.lblInputPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInputPath.ForeColor = System.Drawing.Color.FloralWhite;
+            this.lblInputPath.Location = new System.Drawing.Point(20, 20);
+            this.lblInputPath.Name = "lblInputPath";
+            this.lblInputPath.Size = new System.Drawing.Size(90, 13);
+            this.lblInputPath.TabIndex = 0;
+            this.lblInputPath.Text = "Input File Path";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.txtNoOfrecords);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtOutputFileDelimeter);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txtOutputFilePath);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.ForeColor = System.Drawing.Color.FloralWhite;
+            this.panel2.Location = new System.Drawing.Point(12, 146);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(775, 136);
+            this.panel2.TabIndex = 4;
+            // 
+            // txtNoOfrecords
+            // 
+            this.txtNoOfrecords.Location = new System.Drawing.Point(149, 101);
+            this.txtNoOfrecords.Name = "txtNoOfrecords";
+            this.txtNoOfrecords.Size = new System.Drawing.Size(143, 20);
+            this.txtNoOfrecords.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label4.Location = new System.Drawing.Point(21, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "No. of Records/File";
+            // 
+            // txtOutputFileDelimeter
+            // 
+            this.txtOutputFileDelimeter.Location = new System.Drawing.Point(148, 58);
+            this.txtOutputFileDelimeter.Name = "txtOutputFileDelimeter";
+            this.txtOutputFileDelimeter.Size = new System.Drawing.Size(143, 20);
+            this.txtOutputFileDelimeter.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label2.Location = new System.Drawing.Point(20, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output File Delimeter";
+            // 
+            // txtOutputFilePath
+            // 
+            this.txtOutputFilePath.Location = new System.Drawing.Point(148, 13);
+            this.txtOutputFilePath.Name = "txtOutputFilePath";
+            this.txtOutputFilePath.Size = new System.Drawing.Size(603, 20);
+            this.txtOutputFilePath.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FloralWhite;
+            this.label3.Location = new System.Drawing.Point(20, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Output File Path";
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.btnReset);
+            this.panel3.Controls.Add(this.btnSplit);
+            this.panel3.Location = new System.Drawing.Point(13, 309);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(774, 49);
+            this.panel3.TabIndex = 5;
+            // 
+            // btnReset
+            // 
+            this.btnReset.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnReset.Location = new System.Drawing.Point(288, 13);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(198, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.ForeColor = System.Drawing.Color.DarkBlue;
+            this.btnSplit.Location = new System.Drawing.Point(22, 14);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(198, 23);
+            this.btnSplit.TabIndex = 0;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // prgsBar
+            // 
+            this.prgsBar.Location = new System.Drawing.Point(13, 379);
+            this.prgsBar.Name = "prgsBar";
+            this.prgsBar.Size = new System.Drawing.Size(775, 23);
+            this.prgsBar.TabIndex = 6;
+            this.prgsBar.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(624, 410);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Developed By: Sumit Jangir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(800, 432);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.prgsBar);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Chop Chop(App to Split CSV)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtInputFilePath;
+        private System.Windows.Forms.Label lblInputPath;
+        private System.Windows.Forms.TextBox txtInputFileDelimeter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtNoOfrecords;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtOutputFileDelimeter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutputFilePath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.ProgressBar prgsBar;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CSVBreaker/Form1.cs
@@ -0,0 +1,92 @@
+﻿using CsvHelper;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CSVBreaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                prgsBar.Visible = true;
+
+                prgsBar.Maximum = 100;
+                prgsBar.Step = 1;
+                prgsBar.Value = 1;
+
+                int MAX = Convert.ToInt32(txtNoOfrecords.Text.Trim());
+
+                using (var reader = new StreamReader(txtInputFilePath.Text.Trim()))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    string outFilePath = txtInputFilePath.Text.Trim().Substring(txtInputFilePath.Text.Trim().LastIndexOf("\\") + 1);
+                    outFilePath = outFilePath.Substring(0, outFilePath.Count() - 4);
+
+                    csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header;
+                    csv.Configuration.HeaderValidated = null;
+                    csv.Configuration.Delimiter = txtInputFileDelimeter.Text.Trim();
+                    csv.Configuration.BadDataFound = null;
+
+                    var records = csv.GetRecords<dynamic>().ToList();
+                    int range = 0;
+
+                    if (records.Count > MAX)
+                    {
+                        range = Convert.ToInt32(Math.Ceiling(Convert.ToDecimal(records.Count) / MAX));
+                    }
+
+                    prgsBar.Value = 100 - range;
+
+                    int start = 0, end = 0;
+
+                    for (int i = 0; i < range; i++)
+                    {
+                        start = MAX * i + (i == 0 ? 0 : 1);
+                        end = records.Count - start >= MAX ? MAX : records.Count - start;
+                        var r = records.GetRange(start, end);
+
+
+                        using (var writer = new StreamWriter($"{txtOutputFilePath.Text.Trim()}{outFilePath}{i + 1}.csv"))
+                        using (var new_csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                        {
+                            new_csv.Configuration.Delimiter = txtOutputFileDelimeter.Text.Trim();
+                            new_csv.WriteRecords(r);
+                        }
+                        prgsBar.Value++;
+                    }
+
+                    prgsBar.Value = 100;
+                    MessageBox.Show("Split completed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtOutputFilePath.Text = "";
+            txtInputFilePath.Text = "";
+            txtInputFileDelimeter.Text = "";
+            txtNoOfrecords.Text = "";
+            txtOutputFileDelimeter.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
